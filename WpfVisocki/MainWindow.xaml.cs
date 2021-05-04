0bf69923f91f9f41d2b7d3f8b0dc870c7efcd63a
Gleb Pautov <pautov.split@mail.ru>
2021-05-04 15:06:50 +0500

    done

--- WpfVisocki/MainWindow.xaml.cs
+++ WpfVisocki/MainWindow.xaml.cs
@@ -31,32 +31,29 @@ namespace WpfVisocki
             string urlVar = urlBox.Text;
 
             string[] lines = File.ReadAllLines(urlVar, Encoding.UTF8);
-            //Console.Write("Введите теги через запятую\n>");
             string sub = findBox.Text;
 
             Dictionary<int, int> spisok = new Dictionary<int, int>();
             int count;
 
-            //предсказатель погоды
             for (int i = 0; i < lines.Length; i++)
             {
                 count = 0;
                 string[] temp = sub.Split(',').Select(x => x.Trim()).ToArray(); //разделяем строку запятыми, выбираем каждый элемент и убираем из него пробелы
                 foreach (var str in temp)//пробегаем по полученному массиву подстрок
-                    if (lines[i].Contains(str)) //если в i-й строке содержится подстрока str,
-                        count++;//то прибавляем счетчик
-                spisok.Add(i, count);//добавляем элемент в словарь с ключом i и значением count
+                {
+                    //если в i-й строке содержится подстрока str
+                    if (lines[i].Contains(str)) 
+                    {
+                        output.Items.Add("Найдено совпадение в строке №" + (i + 1) + ":\n" + lines[i]);
+                    }
+                }
             }
+        }
 
-            foreach (var i in spisok)
-            {
-                //if ((string)i.Value == findBox.Text)
-                int a = i.Value;
-                int b = i.Key;
-                output.Items.Add(i.Key);
-                output.Items.Add(i.Value);
-            }
-                //Console.WriteLine("стр. {0} ({1} совп.)", i.Key, i.Value);
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
